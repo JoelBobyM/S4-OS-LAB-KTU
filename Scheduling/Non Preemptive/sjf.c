e6c5cc50526069d722416bf280a1247e56d5a2e2
JoelBobyM <joelbobym@gmail.com>
2021-09-15 22:00:31 +0530

    Shortest Job First

--- Scheduling/Non Preemptive/sjf.c
+++ Scheduling/Non Preemptive/sjf.c
@@ -1,13 +1,19 @@
-include<stdio.h>
+#include<stdio.h>
 #include<string.h>
 struct process
 {
   char name[20];
-  int at,tt,bt,wt,ct,st;
+  int at,tt,bt,wt,status,ct;
 }p[20],temp;
+struct done
+{
+  char name[20];
+  int st,ct;
+}d[20];
 void main()
 {
-  int n,i,tmp,low,j;
+  int n,i,j,ls,min,fnd,num,idle;
+  float twt=0.0,ttt=0.0;
   printf("ENTER THE NUMBER OF PROCESSES : ");
   scanf("%d",&n);
   for(i=0;i<n;i++)                         //Input process details
@@ -19,40 +25,77 @@ void main()
     scanf("%d",&p[i].at);
     printf("BURST TIME : ");
     scanf("%d",&p[i].bt);
+    p[i].status = 0;
   }
-  for(i=0;i<n;i++)                       //Sorting based on at
+  for(i=0,ls=0,num=0,idle=0;ls<n;)
   {
-    for(j=0;j<n-i-1;j++)
+    for(j=0,fnd=0;j<n;j++)
     {
-      if(p[j].at > p[j+1].at)
+      if(i>=p[j].at && p[j].status==0)
       {
-        temp = p[j];
-        p[j] = p[j+1];
-        p[j+1] = temp;
+        if(fnd==0)
+        {
+          min = j;
+          fnd = 1;
+        }
+        else if(fnd!=0 && p[min].bt>p[j].bt)
+        {
+          min = j;
+        }
       }
     }
-  }
-  p[0].ct = p[0].at + p[0].bt;
-  p[0].tt = p[0].ct - p[0].at;
-  p[0].wt = p[0].tt - p[0].bt;
-  for(i=1;i<n;i++)
-  {
-    tmp = p[i-1].ct;
-    low = p[i].bt;
-    for(j=i;j<n;j++)
+    if(idle==0 && fnd==0)
     {
-      if(tmp>=p[j].at && low>=p[j].bt)
-      {
-        low  = p[j].bt;
-        val = j;
-      }
+      strcpy(d[num].name,"Idle");
+      d[num].st = i;
+      i++;
+      idle = 1;
     }
-    p[val].ct = tmp + p[val].bt;
-    p[val].tt = p[val].ct - p[val].at;
-    p[val].wt = p[val].tt - p[val].bt;
-
-    temp = p[val];
-    p[val] = p[i];
-    p[i] = temp;
+    else if(fnd==1)
+    {
+      if(idle==1)
+      {
+        d[num].ct = i;
+        num++;
+        idle = 0;
+      }
+      strcpy(d[num].name,p[min].name);
+      p[min].status =1;
+      d[num].st = i;
+      d[num].ct = i + p[min].bt;
+      i = d[num].ct;
+      p[min].ct = d[num].ct;
+      p[min].tt = p[min].ct - p[min].at;
+      p[min].wt = p[min].tt - p[min].bt;
+      num++;
+      ls++;
+    }
+    else
+    {
+      i++;
+    }
   }
+  printf("\nPROCESS NAME\tCOMPLETION TIME (ms)\tWAITING TIME (ms)\tTURNAROUND TIME (ms)\n\n");
+  for(i=0;i<n;i++)
+  {
+    printf("    %s\t\t\t%d\t\t\t%d\t\t\t%d\n",p[i].name,p[i].ct,p[i].wt,p[i].tt);
+    twt+=p[i].wt;
+    ttt+=p[i].tt;
+  }
+  printf("\n\nGANTT CHART ");
+  printf("\n--------------------------------------------------------------------\n");
+  for(i=0;i<num;i++)
+  {
+    printf("|");
+    printf("%s\t",d[i].name);
+  }
+  printf(" |");
+  printf("\n--------------------------------------------------------------------\n");
+  for(i=0;i<num;i++)
+  {
+      printf("%d\t",d[i].st);
+  }
+  printf("%d\t\n",d[num-1].ct);
+  printf("\nAVERAGE WAITING TIME : %f",(twt/n));
+  printf("\nAVERAGE TURNAROUND TIME : %f\n",(ttt/n));
 }
